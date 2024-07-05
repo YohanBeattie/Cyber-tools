bbb7cf601eb09fe2cc4aee8697f8b7081ab1015a
YohanBeattie <ybeattie076@headmind.com>
2024-07-05 16:58:40 +0200

    [feat] New files openning urls in browser from list

--- home_passwd_gen.py
+++ home_passwd_gen.py
@@ -20,46 +20,56 @@ def main():
     args = parse()
     ####################################################################
     #Insert a sample of words in the above lists
-    words1 = ['HMP', 'HeadMind', 'HEADMIND', 'headmind', 'HEADMIND', 'Headmind']  
-    words2 = ['']
-    date = ['2020', '2021', '2022', '2023', '2024', '']
-    spe_char = ['$', '!', '?', '%', '&', '=', '#', '_', '']
+    words1 = ['Total', 'TOTAL', 'total','T']
+    words2 = [ 'Energies', 'energies','']#'Paribas', 'p', 'P', 'paribas', '']
+    date = ['2019', '2020', '2021', '2022', '2023', '2024', '']
+    spe_char = ['$', '!', '&', '=', '#', '_', '$', '']
     ######################################################################
     passwd_little = []
     print('Launching permutations')
     for element in list(itertools.permutations(spe_char, 2)):
         for word1 in words1:
             for word2 in words2:
-                new_words = [word1] + [word2] + ['2019']
+                new_words = [word1+word2] + ['2019']
                 passwd_little += [''.join(list(mypermut)) for mypermut in list(itertools.permutations(new_words))]
                 new_words+= list(element)
                 passwd_little += [''.join(list(mypermut)) for mypermut in list(itertools.permutations(new_words))]
 
     #print(passwd_no_date[0:10])
     passwd = passwd_little
-    print('Launching character replacement')
-    for password in passwd_little:
-        if '2019' in password:
-            for year in date:
-                passwd.append(password.replace('2019', year))
-        for passwd_iterator in replacer('a', '@', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
-        for passwd_iteraotr in replacer('o', '0', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
-        for passwd_iteraotr in replacer('O', '0', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
-        for passwd_iteraotr in replacer('A', '@', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
-        for passwd_iteraotr in replacer('A', '4', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
-        for passwd_iteraotr in replacer('l', '1', password, 0):
-            if passwd_iterator != password:
-                passwd.append(passwd_iterator)
+    print('Launching character replacement on '+str(len(passwd_little)))
+    i = 1
+    print(passwd_little)
+    with open(args.output, 'w', encoding='utf-8') as g:
+        for password in passwd_little:
+            #print(str(i) + '/'+str(len(passwd)))
+            if '2019' in password:
+                for year in date:
+                    passwd.append(password.replace('2019', year))
+            for passwd_iterator in replacer('a', '@', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('o', '0', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('O', '0', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('A', '@', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('A', '4', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('l', '1', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('s', '$', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
+            for passwd_iterator in replacer('S', '$', password, 0):
+                if passwd_iterator != password:
+                    g.write(passwd_iterator+'\n')
     print('Launching writting output')
 
     with open(args.output, 'w', encoding='utf-8') as g:
---
+++ open_ip_in_browser.py
@@ -0,0 +1,35 @@
+import webbrowser
+import argparse
+
+def parse():
+    '''This function defines the argument of our script'''
+    parser = argparse.ArgumentParser(
+        prog="Open browser with all the pages",
+        description="Basically opens http and https for all IP:Port given",
+    )
+    parser.add_argument("-f", "--file", help="IP file (IP:Port one by line)", required=True)
+    #parser.add_argument("-o", "--output", help="Output file", required=False)
+    return parser.parse_args()
+
+def open_urls(urls):
+    '''This function opens a list of url in your favorite browser'''
+    for url_id in range(len(urls)) :
+        if url_id < 20:
+            webbrowser.open(urls[url_id], new=2)
+        else:
+            input('Press any key to open the next 20 pages')
+            open_urls(urls[20:])
+            break
+    return 0
+
+def main():
+    '''This core fucntion gather the ip:port given, build urls and open them'''
+    args = parse()
+    with open(args.file, 'r', encoding='utf-8') as f:
+        ip_ports = [line.strip() for line in f.readlines()]
+        urls = ["http://"+ip_port+'/' for ip_port in ip_ports]+["https://"+ip_port+'/' for ip_port in ip_ports]
+        open_urls(urls)
+        print("END")
+
+if __name__=='__main__':
+    main()
