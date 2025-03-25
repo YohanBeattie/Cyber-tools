0bca57dfb6ad1b41fcec0ec4842fee2aa10b649f
YohanBeattie <68608803+YohanBeattie@users.noreply.github.com>
2025-03-25 10:10:22 +0000

    [fix] Running msfdb as a non-root user

--- start_msfdb.sh
+++ start_msfdb.sh
@@ -1,15 +1,15 @@
 # This script launch postgreslql and then create or go to the right workspace
 
-if [ "$(id -u)" != "0" ]; then
-    echo "You must be root to do this." 1>&2
-    exit 100
-fi
-if [ "$#" != "1" ]; then
-    echo "Please enter the name of the mission in parameter"
-    exit 100
-fi
+#if [ "$(id -u)" != "0" ]; then
+#    echo "You must be root to do this." 1>&2
+#    exit 100
+#fi
+#if [ "$#" != "1" ]; then
+#    echo "Please enter the name of the mission in parameter"
+#    exit 100
+#fi
 
-systemctl start postgresql
+sudo systemctl start postgresql
 msfdb init
 msfconsole -x "db_status; workspace -a $1"
 
