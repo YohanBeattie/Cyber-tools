90a2ec978ff458e9da3af30733f455b2bd98c9af
yohan <yohan.beattie@gmail.com>
2025-05-08 16:34:46 +0200

    [new] adding a configuration file or api storage

---
+++ api_keys.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+'''
+This file gather the secrets use in favicon search through third party tools
+@authors ybeattie
+'''
+SHODAN_API = None
+VT_API = None
+CENSYS_API = None
+BINARY_EDGE_API = None
+HUNTER_HOW_API = None
+CRIMINALIP_API = None
