ffdbed8a4b65f8555a1ede0a7346b214c21b5530
YBeattie <68608803+YohanBeattie@users.noreply.github.com>
2025-05-09 20:05:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install feroxbuster nmap wafw00f python3 python3-venv tree sslscan
 
-python3 -m venv venv
-source ./venv/bin/activate
+python3 -m venv .venv_cyber_tools
+source ./.venv_cyber_tools/bin/activate
 python3 -m pip install -r requirements.txt
