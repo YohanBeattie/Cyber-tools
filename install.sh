a35ef04183b0fa34f84249b191d88ecad9c64c7b
yohan <yohan.beattie@gmail.com>
2025-06-20 12:11:38 +0200

    [feat] Support install on Manjaro and Arch

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-sudo apt install feroxbuster nmap wafw00f python3 python3-venv tree sslscan
+if command -v apt &> /dev/null; then
+   sudo apt install feroxbuster nmap wafw00f python3 python3-venv tree sslscan
+elif command -v yay &> /dev/null; then
+    yay -Syu --noconfirm feroxbuster nmap wafw00f python3 python3-venv tree sslscan
+else
+    echo "Votre gestionnaire de paquet n'est pas géré. Veuillez installer feroxbuster nmap wafw00f python3 python3-venv tree sslscan"
+    exit 1
+fi
+
 
 python3 -m venv .venv_cyber_tools
 source ./.venv_cyber_tools/bin/activate
 python3 -m pip install -r requirements.txt
+mkdir tmp
